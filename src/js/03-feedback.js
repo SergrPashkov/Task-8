32d355b8b67383e0cdbc94a1ef779762fbf27744
SergrPashkov <128225830+SergrPashkov@users.noreply.github.com>
2023-05-08 01:08:42 +0300

    fixe

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -7,16 +7,6 @@ formEl.addEventListener("input", throttle(() => {
 	localStorage.setItem("feedback-form-state", JSON.stringify({email: email.value, message: message.value}));
 }, 500));
 
-const localStoregGet = JSON.parse(localStorage.getItem("feedback-form-state", JSON.stringify({email: email.value, message: message.value})));
-
-console.log(localStoregGet);
-if (localStoregGet === null){
-	return;
-} else {
-	email.value = localStoregGet.email;
-	message.value = localStoregGet.message;
-}
-
 formEl.addEventListener("submit", (event) => {
 	event.preventDefault();
 	const {email, message } = event.currentTarget.elements;
@@ -24,3 +14,20 @@ formEl.addEventListener("submit", (event) => {
 		event.currentTarget.reset();
 		localStorage.clear();
 	});
+
+const localStoregGet = JSON.parse(localStorage.getItem("feedback-form-state", JSON.stringify({email: email.value, message: message.value})));
+
+function getLocalStoreg(){
+		email.value = localStoregGet.email;
+		message.value = localStoregGet.message;
+}
+
+	const load = () => {
+		try {
+			getLocalStoreg()
+		} catch (error) {
+			
+		}
+	};
+
+	load();
